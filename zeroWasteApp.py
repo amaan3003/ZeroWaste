90230a855b74edece297ce19de9982109a97bb8d
amaan <mohdamaan3003@gmail.com>
2026-02-23 08:25:58 +0000

    Initial interactive layout using Streamlit presets

--- zeroWasteApp.py
+++ zeroWasteApp.py
@@ -6,6 +6,25 @@ import streamlit as st
 import ollama                          
 
 
-st.header("Zero-Waste Recipie Suggester")
+
+st.set_page_config(page_title="Zero-Waste", page_icon="🍲")
+
+st.title("ZERO WASTE RECIPIES 🍲")
 
 
+with st.container(border=True):
+    st.subheader("Input Ingredients")
+    user_input = st.text_area(
+        "What's in your kitchen? (with quantity)",
+        placeholder="e.g., spinach:1, tomato:3",
+        height=100
+    )
+    
+    generateButton = st.button("🍳 Generate Recipes", use_container_width=True, type="primary")
+
+
+if generateButton:
+    with st.status("Dolphin is analyzing...", expanded=True) as status:
+        st.write("Analyzing ingredients...")
+        st.write("Contacting LLM...")
+        status.update(label="Recipes Ready!", state="complete", expanded=False)g
